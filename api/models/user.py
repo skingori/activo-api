0caa4f49ecbf180f1b8ab1d3b5f02297946674f0
Justin Nebo <nebojustin@gmail.com>
2018-06-04 14:59:08 +0100

    chore(validation): add marshmallow schema for all model classes
    [Maintains #157767356]

--- api/models/user.py
+++ api/models/user.py
@@ -7,14 +7,11 @@ from .base.base_model import BaseModel
 class User(BaseModel):
     """Class for user db table."""
 
-    def create_user(self, name, email, image_url=None):
-        """ Method to create a user object instance. """
-        self.name = name
-        self.email = email
-        self.image_url = image_url if image_url else None
-        return self
-
     __tablename__ = 'users'
+
     name = db.Column(db.String(60), nullable=False)
     email = db.Column(db.String(60), nullable=False, unique=True)
     image_url = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User {self.name}>'
