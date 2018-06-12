9571018c2319cf10547454b6fbe12ccec51a0399
SEUNAGBEYE <agbeyeseun1@gmail.com>
2018-06-12 23:31:31 +0100

    chore(test): create token generator
    -add docstrings
    - add the strings utf-8, api/v1 to the constant module to replace all their occurrences
    
    [Start #158285755]

--- tests/helpers/generate_token.py
+++ tests/helpers/generate_token.py
@@ -1,17 +1,26 @@
 """Module for token generation"""
 
 import jwt
-from os import getenv as env
 from base64 import b64encode
 from ..mocks.user import user_one
 from os import getenv
 from base64 import b64decode, encode
 from datetime import datetime
+from api.utilities.constants import UTF_8
 
 
 def generate_token(exp=None):
+    """
+    Generates jwt tokens for testing purpose
+
+    params:
+        exp: Token Expiration. This could be datetime object or an integer
+    result:
+        token: This is the bearer token in this format 'Bearer token'
+    """
+
     secret_key = getenv('JWT_SECRET_KEY')
     payload = {'userInfo': user_one.to_dict()}
     payload.__setitem__('exp', exp) if exp is not None else ''
-    token = jwt.encode(payload, secret_key, algorithm='RS256').decode('utf-8')
+    token = jwt.encode(payload, secret_key, algorithm='RS256').decode(UTF_8)
     return 'Bearer {0}'.format(token)
--- tests/test_token_required_middleware.py
+++ tests/test_token_required_middleware.py
@@ -7,11 +7,12 @@ from api.utilities.constants import (
     INVALID_TOKEN_MSG
 )
 from .helpers.generate_token import generate_token
+from api import api_v1_base_url
 
 
 class TestTokenRequiredMiddleware:
     def test_token_required_when_a_token_does_not_contain_bearer(self, client):
-        response = client.get('/api/v1/asset-categories/stats',
+        response = client.get(f'{api_v1_base_url}/asset-categories/stats',
                               headers={'Authorization': 'bb'})
         response_json = json.loads(response.data.decode('utf-8'))
 
@@ -19,7 +20,7 @@ class TestTokenRequiredMiddleware:
         assert response_json['message'] == NO_BEARER_MSG['message']
 
     def test_token_required_with_invalid_token(self, client):
-        response = client.get('/api/v1/asset-categories/stats',
+        response = client.get(f'{api_v1_base_url}/asset-categories/stats',
                               headers={'Authorization': 'Bearer invalid_token'})
         response_json = json.loads(response.data.decode('utf-8'))
 
@@ -27,7 +28,7 @@ class TestTokenRequiredMiddleware:
         assert response_json['message'] == INVALID_TOKEN_MSG['message']
         
     def test_token_required_with_expired_token(self, client):
-        response = client.get('/api/v1/asset-categories/stats',
+        response = client.get(f'{api_v1_base_url}/asset-categories/stats',
                               headers={'Authorization': generate_token(0)})
         response_json = json.loads(response.data.decode('utf-8'))
 
